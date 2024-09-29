7da33c8a72f0da8f64b09fbc948ba3b624af32d8
noorImen <125492498+noorImen@users.noreply.github.com>
2024-09-29 13:58:01 +0100

    Add files via upload

---
+++ CentralDAchatApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.central_d_achat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDAchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralDAchatApplication.class, args);
+    }
+
+}
